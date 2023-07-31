dad1d30639e73f04dfe0487f01a427bbd685c1c1
MyFRA <tomyntapss@gmail.com>
2023-07-31 19:55:27 +0700

    feat: add dashboard recap

--- src/pages/panel/DashboardPage.jsx
+++ src/pages/panel/DashboardPage.jsx
@@ -7,10 +7,26 @@ import axios from "axios"
 export default function DashboardPage() {
 
     const [user, setUser] = useState({})
+    const [dashboardRecap, setDashboardRecap] = useState({})
 
     useEffect(() => {
         loadUser()
+        loadDashboardRecap()
     }, [])
+
+    const loadDashboardRecap = () => {
+        axios.get(APP_CONFIG.API_URL + 'auth/dashboard', {
+            headers: {
+                Authorization: 'Bearer ' + TokenUtil.getApiToken()
+            }
+        }).then((res) => {
+            setDashboardRecap(res.data.data)
+        }).catch((err) => {
+            ResponseHandler.errorHandler(err)
+        })
+    }
+
+
 
     const loadUser = () => {
         axios.get(APP_CONFIG.API_URL + 'auth/user', {
@@ -44,6 +60,54 @@ export default function DashboardPage() {
             <div className="row">
                 <div className="col">
                     <h3 className='mb-3'>Dashboard</h3>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-lg-4">
+                    <div className="card prod-p-card background-pattern">
+                        <div className="card-body">
+                            <div className="row align-items-center m-b-0">
+                                <div className="col">
+                                    <h6 className="m-b-5">Jumlah Karyawan</h6>
+                                    <h3 className="m-b-0">{dashboardRecap.amount_employees}</h3>
+                                </div>
+                                <div className="col-auto">
+                                    <i className="fas fa-users text-primary"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-4">
+                    <div className="card prod-p-card bg-primary background-pattern-white">
+                        <div className="card-body">
+                            <div className="row align-items-center m-b-0">
+                                <div className="col">
+                                    <h6 className="m-b-5 text-white">Jumlah Cabang</h6>
+                                    <h3 className="m-b-0 text-white">{dashboardRecap.amount_branches}</h3>
+                                </div>
+                                <div className="col-auto">
+                                    <i className="fas fa-building text-white"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-4">
+                    <div className="card prod-p-card background-pattern">
+                        <div className="card-body">
+                            <div className="row align-items-center m-b-0">
+                                <div className="col">
+                                    <h6 className="m-b-5">Karyawan Absensi Hari Ini</h6>
+                                    <h3 className="m-b-0">{dashboardRecap.amount_daily_attendances}</h3>
+                                </div>
+                                <div className="col-auto">
+                                    <i className="fas fa-calendar text-primary"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
